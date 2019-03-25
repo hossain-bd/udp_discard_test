ebac3c020fa6de365d5ee9e75b2f3ffc91dfb847
Nazmul Hossain <hossain-bd@users.noreply.github.com>
2019-03-25 16:04:18 +0100

    Update common.h

--- common.h
+++ common.h
@@ -28,6 +28,8 @@
 #include <pthread.h> 
 
 #include "my_ping.cpp"
+#include<netinet/ip.h>
+#include<netinet/ip_icmp.h>
 
 using namespace std; 
 
@@ -48,64 +50,64 @@ using namespace std;
 
 // Data (1442) + UDP (8) + IP (20) + [ Ethernet (14) + (4) ] = 1484 bytes
 
-bool 			    check_ping;
+bool 			check_ping;
 
-char 			    arg_ip[] = "-ip", 
-                    arg_samples[] = "-s", 
-                    arg_help[] = "--help", 
-                    destination_ip[1000],
-                    * strToTest,
-                    * p;
 
-struct timespec     time_start, 
-			        time_end,
-			        time_code_start;
+char 			arg_ip[] = "-ip", 
+			arg_samples[] = "-s", 
+			arg_help[] = "--help", 
+			*p, *strToTest, 
+			destination_ip[1000];
+
+
+struct timespec 	time_start, 
+			time_end,
+			time_code_start;
 
 struct sockaddr_in 	server, 
-		   	        client, 
-		   	        clntIP;
+		   	client, clntIP;
 
-int 			    sockfd, 
-    			    frames_total, 
-    			    i = 0, j = 0, 
-    			    count_time = 1000, // count_time defined in milliseconds
-    			    error_number = 0; 
+int 			sockfd,
+    			frames_total, 
+    			i = 0, j = 0, 
+    			count_time = 1000, 
+    			error_number = 0; // count_time defined in milliseconds
 
-double 			    sample_numbers = -INFINITY;
+double 			sample_numbers = -INFINITY;
 
-pthread_t 		    t [ NUMTHRDS ];
+pthread_t 		t [ NUMTHRDS ];
 
-unsigned long long int 	total_dgram, 
-		       	    count_frame = 0, 
-		       	    old_frame = 0, 
-		       	    delta_frame = 0, 
-		       	    count_loop = 0, 
-		       	    time_intermediate[5000], 
-			        time_start_nsec,
-			        time_end_nsec,
-			        time_code_start_nsec;
+unsigned long long int 	total_dgram, buffer = 0,
+		       	count_frame = 0, 
+		       	old_frame = 0, 
+		       	delta_frame = 0, 
+		       	count_loop = 0, 
+		       	time_intermediate[5000], 
+			time_start_nsec,
+			time_end_nsec,
+			time_code_start_nsec;
 
-char 			    buffer[BUFFER_SIZE], 
-     			    snd_buffer[100000000], 
-     			    ch;
+char 			//buffer, //buffer[BUFFER_SIZE], 
+     			snd_buffer[100000000], 
+     			ch;
 
-float 			    throughput, 
-      			    throughput_Byte, 
-      			    throughput_bit, 
-      			    throughput_Mbit, 
-      			    max_value = 0.00, 
-      			    min_value = 1000.00, 
-      			    total_time, 
-      			    duration, 
-      			    trace=0.00, 
-      			    trace_95=0.00, 
-      			    trace_94=0.00, 
-      			    throughput_intermediate[5000];
+float 			throughput, 
+      			throughput_Byte, 
+      			throughput_bit, 
+      			throughput_Mbit, 
+      			max_value = 0.00, 
+      			min_value = 1000.00, 
+      			total_time, 
+      			duration, 
+      			trace=0.00, 
+      			trace_95=0.00, 
+      			trace_94=0.00, 
+      			throughput_intermediate[5000];
 
 long double 		avg = 0.00, 
-	    		    average = 0.0;
+	    		average = 0.0;
 
-uint64_t 	    	delta_ns;
+uint64_t 		delta_ns;
 
 auto start = std::chrono::system_clock::now();
 
